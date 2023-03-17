6ec501ef642c1c4c374b8e40ea4884e1865bc613
ilyaskalimullinn <ilyaskalimullinn@yandex.ru>
2023-03-17 19:38:10 +0300

    [edit] moved predictions calculation to binary_metrics + added threshold to FullBinaryMetrics

--- metrics/binary_metrics.py
+++ metrics/binary_metrics.py
@@ -3,17 +3,51 @@ from typing import Tuple
 import numpy as np
 
 
-def calc_simple_metrics(gt: np.array, predictions: np.array, classes: iter) -> Tuple[float]:
+class FullBinaryMetrics:
+    threshold: float
+    TP: float
+    TN: float
+    FP: float
+    FN: float
+    accuracy: float
+    precision: float
+    recall: float
+    f1_score: float
+
+    def __init__(self, TP, TN, FP, FN, threshold):
+        self.TP = TP
+        self.TN = TN
+        self.FP = FP
+        self.FN = FN
+
+        self.threshold = threshold
+
+        self.accuracy = (TP + TN) / (TP + TN + FP + FN)
+        if TP == 0:
+            self.precision = 0
+            self.recall = 0
+            self.f1_score = 0
+        else:
+            self.precision = TP / (TP + FP)
+            self.recall = TP / (TP + FN)
+            self.f1_score = 2 * (self.precision * self.recall) / (self.precision + self.recall)
+
+
+def calc_binary_metrics(gt: np.array, confidence: np.array, threshold: float = 0.5,
+                        classes: iter = [0, 1]) -> FullBinaryMetrics:
     """
-    :param gt: general truth
-    :param predictions: predictions of the model, contains classes
-    :param classes: list of size 2 that contains the classes of the objects (e.g. [0, 1])
-    :return: TP, TN, FP, FN
+    :param gt: general truth, actual classes
+    :param confidence: confidence of the model that the object belongs to class 1
+    :param classes: list of size 2 that contains the classes of the objects (default [0, 1])
+    :param threshold: threshold for defining the class (values >= threshold are considered class 1)
+    :return: full metrics
     """
+
+    predictions = np.where(confidence > threshold, 1, 0)
+
     TP = np.sum(gt[gt == classes[1]] == predictions[gt == classes[1]])
     TN = np.sum(gt[gt == classes[0]] == predictions[gt == classes[0]])
     FP = np.sum(gt[gt == classes[0]] != predictions[gt == classes[0]])
     FN = np.sum(gt[gt == classes[1]] != predictions[gt == classes[1]])
 
-    return TP, TN, FP, FN
-
+    return FullBinaryMetrics(TP, TN, FP, FN, threshold)
